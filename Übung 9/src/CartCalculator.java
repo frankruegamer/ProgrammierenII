b9d4706367d0eeff79b8e936abe3e5fc1e50bf35
Frank <frankruegamer@gmail.com>
2016-06-12 18:34:04 +0200

    Finish exercise 9

---
+++ Übung 9/src/CartCalculator.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class CartCalculator extends AbstractCartCalculator {
+
+    public static void main (String[] args)
+    {
+        new CartCalculator().test();
+    }
+
+    @Override
+    public double totalPrice (InputStream in)
+    {
+        if (in == null) return 0.0;
+
+        DataInputStream dis = new DataInputStream(in);
+        double totalPrice = 0;
+
+        try
+        {
+            while (dis.available() > 0)
+            {
+                int _artnr = dis.readInt();  // ignored
+                int anz = dis.readInt();
+                double price = dis.readDouble();
+
+                totalPrice += anz * price;
+            }
+
+            dis.close();
+        }
+        catch (IOException ignored) { }
+
+        return totalPrice;
+    }
+}
