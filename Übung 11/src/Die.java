c29e920a3a7af3f98adb808b36bfe50745da714b
Frank <frankruegamer@gmail.com>
2016-06-26 19:40:15 +0200

    Finish exercise 11

--- Übung 11/src/Die.java
+++ Übung 11/src/Die.java
@@ -1,2 +1,27 @@
-public class Die {
+import java.util.Iterator;
+
+public class Die extends AbstractDie {
+
+    public static void main (String[] args)
+    {
+        test(new Die());
+    }
+
+    @Override
+    public Iterator<Integer> iterator ()
+    {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext ()
+            {
+                return true;
+            }
+
+            @Override
+            public Integer next ()
+            {
+                return (int) (Math.random() * 6 + 1);
+            }
+        };
+    }
 }
