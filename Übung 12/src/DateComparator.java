645679a7a5513813292a7933ecb3d6091ec7eda5
Frank <frankruegamer@gmail.com>
2016-07-06 23:26:51 +0200

    Remove unnecessary boxing of ints

--- Übung 12/src/DateComparator.java
+++ Übung 12/src/DateComparator.java
@@ -5,11 +5,11 @@ public class DateComparator implements Comparator<Birthday> {
     @Override
     public int compare (Birthday b1, Birthday b2)
     {
-        int result = new Integer(b1.getMonth()).compareTo(new Integer(b2.getMonth()));
+        int result = new Integer(b1.getMonth()).compareTo(b2.getMonth());
         if (result == 0)
-            result = new Integer(b1.getDay()).compareTo(new Integer(b2.getDay()));
+            result = new Integer(b1.getDay()).compareTo(b2.getDay());
         if (result == 0)
-            result = new Integer(b1.getYear()).compareTo(new Integer(b2.getYear()));
+            result = new Integer(b1.getYear()).compareTo(b2.getYear());
         return result;
     }
 }
